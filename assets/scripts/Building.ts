814ed2a3c1ffbff19879ffe7e0eb1ad11d5c39b2
vlaflya <vladislaw.gul@gmail.com>
2021-11-02 11:32:34 +0300

    sum

--- assets/scripts/Building.ts
+++ assets/scripts/Building.ts
@@ -1,5 +1,5 @@
 
-import { _decorator, Component, Node, sp, loader, assetManager, Sprite, ImageAsset, SpriteFrame, resources, Prefab, tween, instantiate, Vec3, Color, UIOpacity, Button, EventTouch, Touch, UITransform, EventHandler, AudioSource, randomRangeInt } from 'cc';
+import { _decorator, Component, Node, sp, loader, assetManager, Sprite, ImageAsset, SpriteFrame, resources, Prefab, tween, instantiate, Vec3, Color, UIOpacity, Button, EventTouch, Touch, UITransform, EventHandler, AudioSource, randomRangeInt, Tween } from 'cc';
 import { GameStateMachine } from './GameStateMachine';
 import { ChoiceManage } from './ChoiceManage';
 import { BuildingPoint } from './BuildingPoint';
@@ -16,13 +16,12 @@ export class Building extends Component {
     @property({type: Prefab}) tapParticles: Prefab
     @property({type: Prefab}) buildParticles: Prefab
     @property({type: Node}) Zebra: Node = null
-    @property({type: Node}) ZebraEndTarget: Node
+    @property({type: Node}) zebraEndTarget: Node
     @property({type: AudioSource}) shortPhrase: AudioSource = null
     @property({type: AudioSource}) longPhrase: AudioSource = null
     
-    private phraseCount: number = 0
-    private ZebraStartPos: Vec3
-
+    private zebraStartPos: Vec3
+    private startScale: Vec3 = null
     private point: BuildingPoint = null
 
     start(){
@@ -37,68 +36,47 @@ export class Building extends Component {
         name = this.node.parent.parent.name + "-" + this.point.getCount() + "-2"
         if(this.node.getChildByName(name) != null && this.point != null)
             this.longPhrase = this.node.getChildByName(name).getComponent(AudioSource)
+        this.startScale = new Vec3(this.node.scale)
+        
     }
 
     private animate(touch: Touch, event: EventTouch){
         if(!this.interactable)
             return
-        if(!GameStateMachine.Instance.stateMachine.isCurrentState("idleState"))
+        if(!GameStateMachine.Instance.stateMachine.isCurrentState("idleState") && !GameStateMachine.Instance.stateMachine.isCurrentState("animationState"))
             return
-        GameStateMachine.Instance.stateMachine.exitState("animationState")
-        this.Zebra = this.node.getChildByPath("visuals/Mask/zebra")
-        this.ZebraEndTarget = this.node.getChildByPath("visuals/Mask/target")
-        console.log(this.Zebra);
-
-        
-
-        if(this.shortPhrase != null && this.longPhrase != null){
-            switch(this.phraseCount){
-                case 0:{
-                    this.longPhrase.play()
-                    this.phraseCount++
-                    break
-                }
-                case 1:{
-                    this.shortPhrase.play()
-                    this.phraseCount++
-                    break
-                }
-                case 2:{
-                    let r = randomRangeInt(0, 2)
-                    SoundManager.Instance.setRandomZebraSound(this.node)
-                    this.phraseCount = 0
-                    break
-                }
+        let playAnimation: boolean = false
+        if(GameStateMachine.Instance.stateMachine.isCurrentState("idleState")){
+            GameStateMachine.Instance.stateMachine.exitState("animationState")
+            if(this.shortPhrase != null && this.longPhrase != null){
+                playAnimation = SoundManager.Instance.getVoiceQueue(this.shortPhrase, this.longPhrase, this.node)
+            }
+            if(this.point.getCount() == 0){
+                let r = randomRangeInt(0, 3)
+                SoundManager.Instance.setSound("island2_ruin_" + r, this.node)
             }
         }
-        if(this.point.getCount() == 0){
-            let r = randomRangeInt(0, 3)
-            SoundManager.Instance.setSound("island2_ruin_" + r, this.node)
+        if(this.Zebra == null){
+            this.Zebra = this.node.getChildByPath("visuals/Mask/Mask/zebra")
+            if(this.Zebra != null){
+                this.zebraEndTarget = this.node.getChildByPath("visuals/Mask/Mask/target")
+                this.zebraStartPos = new Vec3(this.Zebra.worldPosition)
+            }
         }
+        Tween.stopAllByTarget(this.node)
+        this.node.scale = this.startScale
         tween(this.node)
         .call(() => {
             let prt: Node = instantiate(this.tapParticles)
             prt.parent = this.node
             let pos = new Vec3(touch.getUILocation().x, touch.getUILocation().y, 0)
             prt.worldPosition = pos
-            console.log(prt.name);
         })
         .by(0.1, {scale: new Vec3(-0.05, -0.05, -0.07)}, {easing: 'bounceIn'})
         .by(0.1, {scale: new Vec3(0.05, 0.05, 0.07)}, {easing: 'bounceOut'})
         .call(() => {
-            if(this.Zebra != null){
-                this.ZebraStartPos = new Vec3(this.Zebra.position)
-                //this.Zebra.scale = new Vec3(0,0,0)
-                let anim: sp.Skeleton = this.Zebra.getComponent(sp.Skeleton)
-                anim.setAnimation(0, "Happy", false)
-                tween(this.Zebra)
-                // .to(0.5, {scale: new Vec3(1,1,1)})
-                .delay(2)
-                // .to(0.5, {scale: new Vec3(0,0,0)})
-                .call(() => {
-                    GameStateMachine.Instance.stateMachine.exitState()
-                })
-                .start()
+            if(this.Zebra != null && playAnimation && !this.animating){
+                this.animateZebra(true)
             }
             else{
                 GameStateMachine.Instance.stateMachine.exitState()
@@ -106,7 +84,29 @@ export class Building extends Component {
         })
         .start()
     }
-
+    animating: boolean = false
+    animateZebra(canExit: boolean){
+        this.animating = true
+        //this.Zebra.scale = new Vec3(0,0,0)
+        let anim: sp.Skeleton = this.Zebra.getComponent(sp.Skeleton)
+        anim.setAnimation(0, "Happy", false)
+        this.Zebra.getComponent(sp.Skeleton).setEventListener((x, ev) => {this.listner(x, ev)})
+        tween(this.Zebra)
+        .to(0.5, {worldPosition: this.zebraEndTarget.worldPosition})
+        .delay(2)
+        .to(0.5, {worldPosition: this.zebraStartPos})
+        .call(() => {
+            if(canExit)
+                GameStateMachine.Instance.stateMachine.exitState()
+            this.animating = false
+            })
+        .start()
+    }
+    private listner(x, ev){
+        if(ev.data.name == "peak"){
+            console.log("peak")
+        }
+    }
     public init(isCurrentBuilding: boolean, point: BuildingPoint, build: boolean = false){
         this.point = point
         // this.buildButton = this.node.getChildByName("Marker")
@@ -125,7 +125,7 @@ export class Building extends Component {
             this.buildButton.getComponent(Button).clickEvents.push(event)
             this.buildButton.getComponent(Button).interactable = true
             this.node.getChildByName("Marker").getComponent(Marker).init(true)
-            console.log(this.buildButton.getComponent(Button).clickEvents.length);
+            // console.log(this.buildButton.getComponent(Button).clickEvents.length);
             // this.buildButton.on(Node.EventType.TOUCH_START, this.setChoice, this)
         }
         else{
@@ -157,7 +157,7 @@ export class Building extends Component {
         .delay(3)
         .call(() =>{
             if(this.shortPhrase != null)
-                this.shortPhrase.play()
+                SoundManager.Instance.playUniqLine(this.shortPhrase)
         })
         .delay(1)
         .call(() =>{
--- assets/scripts/SoundManager.ts
+++ assets/scripts/SoundManager.ts
@@ -6,16 +6,18 @@ const { ccclass, property } = _decorator;
 export class SoundManager extends Component {
     @property({type: [AudioClip]}) clips: Array<AudioClip> = []
     @property({type: AudioClip}) zebraSounds: Array<AudioClip> = []
+
     private currentSource: AudioSource = null
     private currentQueueCount: number = 0
     @property({type: JsonAsset}) queueAsset: JsonAsset
-    private queue: Array<string>
+    private queue: Array<string> = []
     public static Instance: SoundManager
     
     onLoad(){
         SoundManager.Instance = this
         let r = randomRangeInt(0, 2)
         this.setSound("island2_hello_" + r, this.node)
+        this.readQueue()
     }
 
     private readQueue(){
@@ -30,6 +32,9 @@ export class SoundManager extends Component {
             st += read.queue[c]
         }
         this.queue.push(st)
+        this.queue.forEach(element => {
+            console.log(element);
+        });
     }
 
     public trySetLine(clip: AudioClip, node: Node): boolean{
@@ -42,21 +47,46 @@ export class SoundManager extends Component {
         this.setLine(clip, node)
     }
     
-    public getVoiceQueue(): number{
+    public getVoiceQueue(short: AudioSource, long: AudioSource, node: Node): boolean{
         let st = this.queue[this.currentQueueCount]
-        switch(st){
-            case("short"):{
-                return 0
-            }
-            case("long"):{
-                return 1
-            }
-            case("zebra"):{
-                return 2
+        let playeAnim: boolean = false
+        if(this.currentSource != null){
+            if(this.currentSource.playing){
+                return true
             }
         }
+        switch(st){
+            case("short"):{
+                this.currentSource = short
+                short.play()
+                this.currentQueueCount++
+                break
+            }
+            case("long"):{
+                this.currentSource = long
+                long.play()
+                this.currentQueueCount++
+                break
+            }
+            case("zebra"):{
+                this.currentSource = this.setRandomZebraSound(node)
+                this.currentQueueCount++
+                playeAnim = true
+                break
+            }
+        }
+        if(this.currentQueueCount == this.queue.length){
+            this.currentQueueCount = 0
+        }
+        return playeAnim
     }
-
+    public playUniqLine(source: AudioSource){
+        if(this.currentSource != null)
+            if(this.currentSource.playing)
+                return
+        this.currentSource = source
+        source.play()
+    }
     private setLine(clip: AudioClip, node: Node){
         let source: Node = null
         source = node.getChildByName("voice")
@@ -66,7 +96,7 @@ export class SoundManager extends Component {
         source = new Node("voice")
         this.setClip(source, clip)
     }
-    public setRandomZebraSound(node: Node){
+    public setRandomZebraSound(node: Node): AudioSource{
         let source: Node = null
         source = node.getChildByName("source")
         if(source != null){
@@ -74,7 +104,7 @@ export class SoundManager extends Component {
         }
         source = new Node("source")
         let r = randomRangeInt(0, this.zebraSounds.length)
-        this.setClip(source, this.zebraSounds[r])
+        return this.setClip(source, this.zebraSounds[r])
     }
     public setSound(name: string, node: Node){
         this.clips.forEach(clip => {
@@ -89,10 +119,11 @@ export class SoundManager extends Component {
             }
         });
     }
-    private setClip(node: Node, clip: AudioClip){
+    private setClip(node: Node, clip: AudioClip): AudioSource{
         let source =  node.addComponent(AudioSource)
         source.clip = clip
         source.play()
+        return source
     }
 }
 interface readQueue{
